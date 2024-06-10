8b05042a7b01ea874663f37de7d90645da39f08c
Saad Anwar <01saadanwar@gmail.com>
2024-06-10 23:20:09 +0500

    first second

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: "https://the-bread-way-53yg.vercel.app/",
+  origin: "https://the-bread-way-53y.vercel.app",
   credentials: true,
 }));
 app.use(express.json());
